4da2723016c54d857fa7b5d1ed362e0379f60cd2
Sahib Aliyev <sahibaliyev@MacBook-Pro.local>
2026-06-24 17:54:57 +0400

    Added Logistic Regression and small refactoring

---
+++ src/input.py
@@ -0,0 +1,40 @@
+import pandas as pd
+import os
+
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
+
+Tfidf_vectorizer = TfidfVectorizer(
+    stop_words="english",
+    lowercase=True,
+    max_features=50000,
+    min_df=2,
+    max_df=0.95
+)
+
+Count_vectorizer = CountVectorizer(
+    stop_words="english", 
+    binary=True, 
+    max_features=50000
+)
+
+
+def get_data(train_percent : float, validation_percent : float, seed : int) -> tuple:
+    data = pd.read_csv(os.path.join('data', 'combined_data.csv'))
+
+    data = data.dropna(subset=["text", "label"])
+    data = data.drop_duplicates(subset=["text"])
+
+    X = data["text"]
+    y = data["label"]
+
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, train_size=train_percent, stratify=y, random_state=seed
+    )
+
+    X_train, X_val, y_train, y_val = train_test_split(
+        X_train, y_train, test_size=validation_percent,
+        stratify=y_train, random_state=seed
+    )
+
+    return X_train, X_val, X_test, y_train, y_val, y_test
--- src/train.py
+++ src/train.py
@@ -1,76 +1,56 @@
-import pandas as pd
-from sklearn.model_selection import train_test_split
+from input import get_data, Tfidf_vectorizer, Count_vectorizer
 
 from sklearn.naive_bayes import MultinomialNB, BernoulliNB, ComplementNB
 from sklearn.linear_model import LogisticRegression
 from sklearn.svm import LinearSVC
 
-from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
 from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
 
 import joblib
 import os
 
+
 SEED = 420
 TRAIN_PERCENT = 0.85
 VALIDATION_PERCENT = 0.01
 
-def get_data() -> tuple:
-    data = pd.read_csv(os.path.join('data', 'combined_data.csv'))
-
-    data = data.dropna(subset=["text", "label"])
-    data = data.drop_duplicates(subset=["text"])
-
-    X = data["text"]
-    y = data["label"]
-
-    X_train, X_test, y_train, y_test = train_test_split(
-        X, y, train_size=TRAIN_PERCENT, stratify=y, random_state=SEED
-    )
-
-    X_train, X_val, y_train, y_val = train_test_split(
-        X_train, y_train, test_size=VALIDATION_PERCENT,
-        stratify=y_train, random_state=SEED
-    )
-
-    return X_train, X_val, X_test, y_train, y_val, y_test
-
-
-X_train, X_val, X_test, y_train, y_val, y_test = get_data()
-
-Tfidf_vectorizer = TfidfVectorizer(
-    stop_words="english",
-    lowercase=True,
-    max_features=50000,
-    min_df=2,
-    max_df=0.95
-)
-Count_vectorizer = CountVectorizer(
-    stop_words="english", 
-    binary=True, 
-    max_features=50000
+X_train, X_val, X_test, y_train, y_val, y_test = get_data(
+    train_percent=TRAIN_PERCENT,
+    validation_percent=VALIDATION_PERCENT,
+    seed=SEED
 )
 
 
-
-bayes_models = {
-    ("Multinomial NB", Tfidf_vectorizer, MultinomialNB()),
-    ("Complement NB", Tfidf_vectorizer, ComplementNB()),
-    ("Bernoulli NB", Count_vectorizer, BernoulliNB())
+models = {
+    'Multinomial_NB': (Tfidf_vectorizer, MultinomialNB()),
+    'Complement_NB': (Tfidf_vectorizer, ComplementNB()),
+    'Bernoulli_NB': (Count_vectorizer, BernoulliNB()),
+    'Logistic_Regression': (Tfidf_vectorizer, LogisticRegression(
+        random_state=SEED, max_iter=1000
+    )),
 }
 
-for name, vectorizer, model in bayes_models:
-    X_train_vector = vectorizer.fit_transform(X_train)
-    X_val_vector = vectorizer.transform(X_val)
-    X_test_vector = vectorizer.transform(X_test)
-    print(name + ":")
+def train_models():
+    for name in models.keys():
+        vectorizer, model = models[name]
 
-    model.fit(X_train_vector, y_train)
-    y_predict = model.predict(X_test_vector)
+        X_train_vector = vectorizer.fit_transform(X_train)
+        X_test_vector = vectorizer.transform(X_test)
 
-    print(confusion_matrix(y_test, y_predict))
-    print(accuracy_score(y_test, y_predict))
-    print(classification_report(y_test, y_predict))
+        print(name + ":")
+        model.fit(X_train_vector, y_train)
+        y_predict = model.predict(X_test_vector)
 
-    joblib.dump(model, os.path.join('models', name + '.pkl'))
-    joblib.dump(vectorizer, os.path.join('models', name + ' vectorizer.pkl'))
+        print(confusion_matrix(y_test, y_predict))
+        print(accuracy_score(y_test, y_predict))
+        print(classification_report(y_test, y_predict))
+
+        joblib.dump(model, os.path.join('models', name + '.pkl'))
+
+
+
+def main():
+    train_models()
+
+if __name__ == '__main__':
+    main()
